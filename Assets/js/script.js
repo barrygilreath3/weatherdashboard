eac98eb7b210f1018d0793f91c4e8c5648d3dc8e
Barry Gilreath <barrygilreath3@gmail.com>
2022-04-09 16:56:03 -0400

    added uvindex

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,9 +4,11 @@ var submitEl = document.querySelector('#submit');
 var cityContainerEl = document.querySelector('#cityCard');
 var fiveDay = document.querySelector('.fiveDay');
 var citySearchTerm = document.querySelector('#city-search-term')
+var cityIcon = document.querySelector('#cityIcon');
 var cityTemp = document.querySelector('#city-temp');
 var cityWind = document.querySelector('#city-wind');
 var cityHumidity = document.querySelector('#city-humidity');
+
 // var UVIndex = document.querySelector('#uvindex');
 // var date = document.querySelector ('')
 
@@ -54,13 +56,50 @@ var displayCity = function (data) {
 
   cityContainerEl.style.display = "block";
   fiveDay.style.display = "block";
+
+  // var buttonBox = document.getElementById('buttonBox')
+  // var btn = getUV (data.coord.lat, data.coord.lon);
+  // // buttonBox.append(btn);
+  // console.log(btn);
+
+    fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+APIKey).then(function(response) {
+      return response.json()
+    }).then(function (results){
+      console.log(results);
+      var uvBtn = document.createElement("button");
+      var btnColor;
+      if (results.current.uvi < 3) {
+        btnColor = 'success';
+      }
+      else if (results.current.uvi > 7) {
+        btnColor = 'danger';
+      }
+      else {
+        btnColor = 'warning';
+      }
+      uvBtn.setAttribute('class', 'btn btn-'+btnColor);
+      uvBtn.textContent = results.current.uvi;
+      var buttonBox = document.getElementById("buttonBox");
+      buttonBox.appendChild(uvBtn);
+    })
+
+  let IconUrl=document.getElementById('cityIcon');
+  IconUrl.setAttribute('src', 'https://openweathermap.org/img/w/'+data.weather[0].icon +'.png')
+  // $('#cityIcon').attr('src', IconUrl);
+  // 'https://openweathermap.org/img/w/'+data.weather[0].icon +'.png'
+  
+
+
+  // cityIcon.textContent = IconUrl.textContent;
   citySearchTerm.textContent = data.name;
   cityTemp.textContent = data.main.temp;
   cityWind.textContent = data.wind.speed;
   cityHumidity.textContent = data.main.humidity;
   // UVIndex.textcontent = 
 
-  for (var i=0; i < 4; i++) {
-    var cityName = city[i] + '/'
-  }
+  // for (var i=0; i < 4; i++) {
+  //   var cityName = city[i] + '/'
+  // }
 }
+
+
