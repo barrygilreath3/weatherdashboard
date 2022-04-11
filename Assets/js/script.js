6b88a88629de7479cfaa44ce948bf7b2ec12cf73
Barry Gilreath <barrygilreath3@gmail.com>
2022-04-11 09:34:37 -0400

    added moment

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,6 @@
 var APIKey = 'd19c1a301d9f7f28233bbb27ec5cb0cd';
 var userCityEl = document.querySelector('#userCity');
+var today = document.querySelector("#today");
 var submitEl = document.querySelector('#submit');
 var cityContainerEl = document.querySelector('#cityCard');
 var fiveDay = document.querySelector('.fiveDay');
@@ -8,6 +9,11 @@ var cityIcon = document.querySelector('#cityIcon');
 var cityTemp = document.querySelector('#city-temp');
 var cityWind = document.querySelector('#city-wind');
 var cityHumidity = document.querySelector('#city-humidity');
+
+var forecastDate = document.querySelector('.forecastDate');
+var forecastTemp = document.querySelector('.forecastTemp');
+var forecastWind = document.querySelector('.forecastWind');
+var forecastHumidity = document.querySelector('.forecastHumidity');
 
 // var UVIndex = document.querySelector('#uvindex');
 // var date = document.querySelector ('')
@@ -57,12 +63,7 @@ var displayCity = function (data) {
   cityContainerEl.style.display = "block";
   fiveDay.style.display = "block";
 
-  // var buttonBox = document.getElementById('buttonBox')
-  // var btn = getUV (data.coord.lat, data.coord.lon);
-  // // buttonBox.append(btn);
-  // console.log(btn);
-
-    fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+APIKey).then(function(response) {
+    fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+data.coord.lat+"&lon="+data.coord.lon+"&appid="+APIKey+"&units=imperial").then(function(response) {
       return response.json()
     }).then(function (results){
       console.log(results);
@@ -81,25 +82,40 @@ var displayCity = function (data) {
       uvBtn.textContent = results.current.uvi;
       var buttonBox = document.getElementById("buttonBox");
       buttonBox.appendChild(uvBtn);
+
+      writeDates (results);
+
+      writefiveDay(results);
+
     })
 
   let IconUrl=document.getElementById('cityIcon');
   IconUrl.setAttribute('src', 'https://openweathermap.org/img/w/'+data.weather[0].icon +'.png')
-  // $('#cityIcon').attr('src', IconUrl);
-  // 'https://openweathermap.org/img/w/'+data.weather[0].icon +'.png'
   
-
-
-  // cityIcon.textContent = IconUrl.textContent;
   citySearchTerm.textContent = data.name;
   cityTemp.textContent = data.main.temp;
   cityWind.textContent = data.wind.speed;
   cityHumidity.textContent = data.main.humidity;
-  // UVIndex.textcontent = 
-
-  // for (var i=0; i < 4; i++) {
-  //   var cityName = city[i] + '/'
-  // }
 }
 
+function writeDates (results) {
+  today.textContent = moment.unix(results.daily[0].dt).format("MM/DD/YYYY");
+  console.log (today.textContent);
 
+}
+
+function writefiveDay (results) {
+
+  forecastDate.textContent = moment.unix(results.daily[1].dt).format("MM/DD/YYYY");
+  forecastTemp.textContent = results.daily[1].temp.max;
+  forecastWind.textContent = results.daily[1].wind_speed;
+  forecastHumidity.textContent = results.daily[1].humidity;
+
+  // var tomorrowIcon=document.getElementById('tomorrowIcon');
+  // tomorrowIcon.setAttribute('src', 'https://openweathermap.org/img/w/'+data.weather[0].icon +'.png')
+  // var tomorrowIcon = document.getElementbyId('tomorrowIcon');
+  // tomorrowIcon.textContent = results.daily[0].weather[0].icon;
+
+  // tomorrowIconUrl.setAttribute('src', 'https://openweathermap.org/img/w'+results.daily[0].weather[0].icon)
+
+}
